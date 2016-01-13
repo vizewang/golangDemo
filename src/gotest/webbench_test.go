0239f59ff8612744e4812c32eabfe13ee34d87b9
weizwang <weizwang@ebay.com>
2016-01-13 16:12:11 +0800

    add demo

---
+++ src/gotest/webbench_test.go
@@ -0,0 +1,17 @@
+package gotest
+import "testing"
+
+func Benchmark_Division(b *testing.B)  {
+	for i:=0;i<b.N;i++{
+		Division(4,5)
+	}
+
+}
+func Benchmark_TimeConsumingFunction(b *testing.B)  {
+	b.StopTimer()
+	b.StartTimer()
+	for i:=0;i<b.N;i++{
+		Division(4,5)
+	}
+
+}
